9b36f92de739ed378115647365208f108bcdfe9d
vituinhu <vitinsouza3108@gmail.com>
2025-02-05 11:39:27 -0300

    Lista07

---
+++ Estrutura condicional/switch.js
@@ -0,0 +1,26 @@
+let opcao = 2
+switch (opcao){
+case 1:
+    console.log("Você escolheu opcao 1")
+    break
+case 2:
+    console.log("Você escolheu opcao 2")
+    break
+default:
+    console.log("Opção Inválida")
+}
+
+let letra = "A"
+switch (opcao){
+case "A":
+    console.log("Vogal")
+    break
+case "B":
+    console.log("Consoante")
+    break
+case "C":
+    console.log("Consoante")
+    break   
+default:
+    console.log("Não é uma letra")
+}
