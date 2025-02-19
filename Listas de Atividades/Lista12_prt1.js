7045394ced1304c1a3e3057044012caaf18718b8
vituinhu <vitinsouza3108@gmail.com>
2025-02-19 10:58:24 -0300

    Update Lista12_prt1.js

--- Listas de Atividades/Lista12_prt1.js
+++ Listas de Atividades/Lista12_prt1.js
@@ -30,10 +30,11 @@ function multiplicarDoisNumeros(a, b){
     let resultado = a * b
     return resultado
 }
-console.log(multiplicarDoisNumeros(5, 3));
+console.log(multiplicarDoisNumeros(5, 10));
 
 // 5
 
+let idade = prompt("Digite sua idade")
 function verificarIdade(idade){
     if(idade >= 18){
         console.log("Maior de idade")
@@ -41,15 +42,18 @@ function verificarIdade(idade){
         console.log("Menor de idade")
     }
 }
-verificarIdade(18);
+verificarIdade(idade);
 
 // 6
 
-function somar (a, b){
-    let resultado = a + b
+let a = Number(prompt ("Digite um número"));
+let b = Number (prompt ("Digite outro número"));
+function somar(a, b){
+    
+    let resultado = a + b 
     return resultado
 }
-console.log(somar(10, 5));
+console.log(somar(a, b))
 
 // 7
 
@@ -61,13 +65,17 @@ console.log(calcularAreaRetangulo(5, 10));
 
 // 8
 
-function compararIdade (idade1, idade2){
-    if(idade1 > idade2){
+function compararIdade(idade1, idade2) {
+    if (idade1 > idade2) {
         console.log("A primeira pessoa é mais velha")
-    } else if(idade1 < idade2){
+    } else if (idade2 > idade1) {
         console.log("A segunda pessoa é mais velha")
     } else {
         console.log("As duas pessoas têm a mesma idade")
     }
 }
-compararIdade(10, 10);
+
+let idade1 = prompt("Digite a idade da primeira pessoa:")
+let idade2 = prompt("Digite a idade da segunda pessoa:")
+
+compararIdade(idade1, idade2);
