7933c3b26edfe88f8325cd06bbb622c717dcb5d7
vituinhu <vitinsouza3108@gmail.com>
2025-03-25 11:01:35 -0300

    .

--- PPT/main.js
+++ PPT/main.js
@@ -11,8 +11,7 @@ function jogar(escolha) {
         ganhador = 0;
    } else if (jogadorEscolha == 1 && computadorEscolha == 2) {
         ganhador = 2;
-   }
-   else if (jogadorEscolha == 1 && computadorEscolha == 3) {
+   } else if (jogadorEscolha == 1 && computadorEscolha == 3) {
         ganhador = 1;
    } else if (jogadorEscolha == 2 && computadorEscolha == 1) {
         ganhador = 1;
@@ -27,4 +26,38 @@ function jogar(escolha) {
    } else if (jogadorEscolha == 3 && computadorEscolha == 3) {
         ganhador = 0;
    }
+
+
+let jogadorEscolha1 = document.getElementById("jogadorEscolha1");
+let jogadorEscolha2 = document.getElementById("jogadorEscolha2");  
+let jogadorEscolha3 = document.getElementById("jogadorEscolha3");
+let computadorEscolha1 = document.getElementById("computadorEscolha1");
+let computadorEscolha2 = document.getElementById("computadorEscolha2");
+let computadorEscolha3 = document.getElementById("computadorEscolha3");
+
+jogadorEscolha1.classList.remove("selecionado");
+jogadorEscolha2.classList.remove("selecionado");
+jogadorEscolha3.classList.remove("selecionado");
+computadorEscolha1.classList.remove("selecionado");
+computadorEscolha2.classList.remove("selecionado");
+computadorEscolha3.classList.remove("selecionado");
+
+document.getElementById("jogadorEscolha" + jogadorEscolha).classList.add("selecionado");
+document.getElementById("computadorEscolha" + computadorEscolha).classList.add("selecionado");
+
+let mensagem = document.getElementById("mensagem");
+if (ganhador == 0) {
+    mensagem.innerText = "Empate!";
+} else if (ganhador == 1) {
+    mensagem.innerText = "Você ganhou!";
+    jogadorPontos++;
+} else if (ganhador == 2) {
+    mensagem.innerText = "Você perdeu!";
+    computadorPontos++;
+}
+
+let pontosJ = document.getElementById("jogadorPontos");
+let pontosC = document.getElementById("computadorPontos");
+pontosJ.innerText = jogadorPontos;
+pontosC.innerText = computadorPontos;
 }
